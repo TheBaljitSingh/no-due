5454842336878c81bbf345898a1faccc64912d8d
Baljit Singh <thebaljitsinghin@gmail.com>
2025-12-08 11:22:53 +0530

    removed auth on my own branch

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -1,22 +1,4 @@
 import express from "express";
-import passport from "passport";
-import { loginUser, registerUser, logoutUser, myProfile, handleGoogleAuth } from "../controller/user.controller.js";
-import {isAuthenticated} from "../middleware/authMiddleware.js"
 
 
 const router = express.Router();
-
-
-router.get("/google", passport.authenticate("google", {scope:["profile","email"]}));
-router.get("/google/callback", passport.authenticate("google", {session:false}), handleGoogleAuth);
-router.post("/login", loginUser);
-router.post("/register", registerUser);
-router.post("/logout", logoutUser);
-router.get("/me", isAuthenticated, myProfile);
-
-//just for testing
-router.get("/dashboard", isAuthenticated, (req, res,next)=>{
-    return res.status(200).json({messgage:"middleware working fine"});
-  })
-
-export default router;

e3bc573d39478ef9fdccf86a58c5307a4e593465
Baljit Singh <thebaljitsinghin@gmail.com>
2026-02-16 13:35:23 +0530

    uncommeted the overdule flow, and created the update password api

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { registerUser, updateUser } from '../controller/user.controller.js';
+import { registerUser, updateUser, updatePassword } from '../controller/user.controller.js';
 
 const router = express.Router();
 
-router.post('/',registerUser);
-router.put('/',updateUser);
+router.post('/', registerUser);
+router.put('/', updateUser);
+router.put('/password', updatePassword);
 
 export default router;
